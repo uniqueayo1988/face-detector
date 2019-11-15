85bb5e5650e8649d3b325f5ec64580b9dc5bcbb4
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-11-15 23:08:43 +0100

    Consume endpoint

--- src/App.js
+++ src/App.js
@@ -40,6 +40,13 @@ class App extends React.Component {
     isSignedIn: false
   }
 
+  componentDidMount () {
+    fetch('http://localhost:8080')
+      .then(response => response.json())
+      .then(data => console.log(data, 'hello....'))
+      // .then(console.log)
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage')
