b0edecd51d9ec85f0a4d0d258af61e70c0ba398d
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-12-04 21:15:19 +0100

    Updates

---
+++ precache-manifest.436c7916b39cab49f8840ef3c23f118a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da225c75c6e7f0e7c75c49c83e31804c",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "4a3fd6610daf89661f68",
+    "url": "/face-detector/static/css/main.02f2ab5e.chunk.css"
+  },
+  {
+    "revision": "7c2f714808800905e8e0",
+    "url": "/face-detector/static/js/2.b4d3d3b9.chunk.js"
+  },
+  {
+    "revision": "4a3fd6610daf89661f68",
+    "url": "/face-detector/static/js/main.376a8bee.chunk.js"
+  },
+  {
+    "revision": "f276adb28ea68e35f7cc",
+    "url": "/face-detector/static/js/runtime-main.4e9d0a86.js"
+  },
+  {
+    "revision": "014560c03cbcab7f1adee1b2824cc5d6",
+    "url": "/face-detector/static/media/brain.014560c0.svg"
+  }
+]);
